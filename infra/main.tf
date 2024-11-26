d3ffe6f6cba16bf68e24af36951c13d8d41729f1
Geoffrey Choy <geoffchoy@outlook.com>
2024-11-26 09:44:10 +0000

    Cache is able to be deployed

---
+++ infra/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.9.8"
+
+  cloud {
+    organization = "personal-burrt"
+    workspaces {
+      name = "shared-services"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.76"
+    }
+  }
+}
+
+module "consul" {
+  source = "git@github.com:burrt/AwsLambdaDotnetWebApi.git"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_elasticache_subnet_group" "shared-redis-cache-subnet-group" {
+  name        = "shared-redis-cache-subnet-group"
+  description = "Redis Cache Subnet Group"
+  subnet_ids = [
+    local.subnet-private-1-id,
+    local.subnet-private-2-id
+  ]
+}
+
+resource "aws_elasticache_cluster" "shared-cache" {
+  cluster_id            = "shared-cache-cluster"
+  engine                = "redis"
+  node_type             = "cache.t2.micro"
+  parameter_group_name  = "default.redis7"
+  num_cache_nodes       = 1
+  port                  = 6379
+  engine_version        = "7.1"
+  subnet_group_name     = aws_elasticache_subnet_group.shared-redis-cache-subnet-group.name
+}
+
+# create security group for the cluster (redis traffic)
+# create security group for the ec2 (ssh traffic)
+# allocate an elastic ip for public IP
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+locals {
+    # subnet IDs
+    subnet-public-1-id = "subnet-032489051af339b0f"
+    subnet-public-2-id = "subnet-09936b1d99c57b6cf"
+    subnet-private-1-id = "subnet-0803188b621d8cfda"
+    subnet-private-2-id = "subnet-04b5c4d447795d6f9"
+
+}
