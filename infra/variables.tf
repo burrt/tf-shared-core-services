934af680bf219d750ec9b30cccdb3f9e4fc318a9
Nazim Ahmed <nazim.ahmed427@gmail.com>
2025-01-25 14:41:07 +0000

    Formating code and adding new files for outputs and versions

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,27 +3,27 @@
 ##############################
 
 variable "my_local_ip" {
-  description   = "My local IP address stored as an environment variable"
-  type          = string
+  description = "My local IP address stored as an environment variable"
+  type        = string
 }
 
 variable "subnet_public_1_id" {
-  description   = "Public subnet 1"
-  type          = string
-  default       = ""
+  description = "Public subnet 1"
+  type        = string
+  default     = ""
 }
 variable "subnet_public_2_id" {
-  description   = "Public subnet 2"
-  type          = string
-  default       = ""
+  description = "Public subnet 2"
+  type        = string
+  default     = ""
 }
 variable "subnet_private_1_id" {
-  description   = "Private subnet 1"
-  type          = string
-  default       = ""
+  description = "Private subnet 1"
+  type        = string
+  default     = ""
 }
 variable "subnet_private_2_id" {
-  description   = "Private subnet 2"
-  type          = string
-  default       = ""
+  description = "Private subnet 2"
+  type        = string
+  default     = ""
 }
